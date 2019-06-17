1776345c7e5f949a21bb8fe0d1792c6ef024b033
Katsuya003 <bb.win.3@gmail.com>
2019-06-18 04:52:54 +0900

    Correct the description.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Video, type: :model do
   describe '#find_all' do
-    it 'test' do
-      result = Video.find_all
-      expect(result.class).to equal(String)
+    it 'can change video data to json.' do
+      expect(Video.find_all.class).to equal(String)
     end
   end
 end
