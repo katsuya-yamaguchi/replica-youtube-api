12c818e653da47ebd3c8839d109488f5059afaac
Katsuya003 <bb.win.3@gmail.com>
2019-06-17 23:17:09 +0900

    Create video controller.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,5 @@
 class VideosController < ApplicationController
+  def index
+    return Video.find_all
+  end
 end
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe VideosController, type: :controller do
-
+  describe '#index' do
+    it 'can get json format data.' do
+      get :index
+      expect(response.status).to eq(204)
+    end
+  end
 end
